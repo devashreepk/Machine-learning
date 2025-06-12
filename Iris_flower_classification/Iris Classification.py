0fbc2cf0faa8000c7eccf1b8106d743e98524f56
devashreepk <devashree59@gmail.com>
2025-06-12 17:12:07 +0100

    refactored sentiment analysis

---
+++ Iris_flower_classification/Iris Classification.py
@@ -0,0 +1,10 @@
+"""Objective
+The aim is to classify Iris flowers among three species from measurements of sepals and petals length and width. The Iris data set contains 3 classes of 50 instances each, where each class refers to a type of Iris plany. The central goal here is to design a model that makes useful classifications for new flowers or, in other words, one which exibits good generalization.</p>
+"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+#import matplotlib.pyplot as plt
+#from sklearn import preprocessing
+
+
